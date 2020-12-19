9b483f8e879faee6115821ff600d94f4e79f18ba
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-12-19 21:11:50 +0100

    Fix header rendering problem

--- components/layout.js
+++ components/layout.js
@@ -6,13 +6,15 @@ export default function Layout({ children }) {
     <>
       <div className="h-full pt-2 xl:pt-24 bg-gradient-to-b from-darkA to-darkB">
         <main className="m-4 mb-6 xl:m-0 xl:mb-12 xl:mx-auto xl:max-w-screen-xl main">
-          <div className="flex flex-col items-center justify-between px-3 pt-6 pb-10 uppercase sm:pt-0 sm:pb-3 sm:flex-row text-yellowLight">
+          <div className="flex flex-col items-center justify-between px-3 pt-6 pb-10 uppercase xl:h-6 lg:pt-0 lg:pb-3 lg:flex-row text-yellowLight">
             <Link href="/">
               <a aria-label="Métiers du numérique">
-                <ProjectName className="w-full h-5 max-w-full overflow-hidden sm:h-3 xl:h-full" />
+                <ProjectName className="h-6 max-w-full overflow-hidden lg:h-full xl:h-full" />
               </a>
             </Link>
-            <CreatorName className="w-full h-3 max-w-full mt-1 overflow-hidden sm:mt-0 xl:h-full" />
+            <div>
+              <CreatorName className="h-full max-w-full mt-1 overflow-hidden lg:mt-0" />
+            </div>
             {/* <span>Métiers du numérique</span>
           <span>vanon borget - Mattèo gauthier</span> */}
           </div>
