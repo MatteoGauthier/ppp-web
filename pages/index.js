374a6b910b35c16ba296eb5a3b4ca3637783e2c8
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-01-04 21:33:13 +0100

    Adding open graph to the metadata

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,22 @@ export default function IndexPage() {
   return (
     <>
       <Head>
-        <meta name="keywords" content="PPP, MMI, DUT, DUT MMI, Mattèo Gauthier, Vanon Borget, Présentation, Product designer, Motion Designer, Vidéaste" />
+        <meta
+          name="keywords"
+          content="PPP, MMI, DUT, DUT MMI, Mattèo Gauthier, Vanon Borget, Présentation, Product designer, Motion Designer, Vidéaste"
+        />
+        <meta property="og:title" content="Présentation de métiers du numérique - Vanon Borget & Mattèo Gauthier" />
+        <meta property="og:site_name" content="Présentation de métiers du numérique" />
+        <meta property="og:url" content="https://ppp-mmi.now.sh/" />
+        <meta
+          property="og:description"
+          content="Présentation de métiers du numérique, Vanon Borget et Mattèo Gauthier allons vous présenter le métier de Product Designer, Motion Designer et Vidéaste"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+        <meta property="og:image" content="/og.png" />
 
         <title>Présentation de métiers du numérique - Vanon Borget & Mattèo Gauthier</title>
       </Head>
